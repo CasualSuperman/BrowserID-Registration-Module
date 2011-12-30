0904d704204b68f52566724dd67061ecda7da470
Bobby Wertman <rwertman@polaris.cs.wcu.edu>
2011-12-29 20:05:43 -0500

    Reworked to have better layout.

--- sha1.js
+++ sha1.js
@@ -1,54 +1,50 @@
 var lib = {
-	fs    : require('fs'),
-	path  : require('path'),
 	crypto: require('crypto'),
 };
 
-var maxCache = -1;
-
-var sha1 = (function() {
-	var noCache = (function() {
-		return function sha1(data) {
-			return lib.crypto.createHash('sha1').update(data).digest('hex');
-		};
-	}());
-	var allCache = (function() {
-		// Cache of hashed emails in a local scope.
-		var hashCache = {};
-		return function sha1(data) {
-			return hashCache[data] || hashCache[data] = noCache(data);
-		};
-	}());
-	var someCache = function makeSomeHash(count) {
-		var hashCache = {
-			size: 0,
-			last: [],
-		};
-		var shrinkToSize = function() {
-			while (hashCache.size > count) {
-				delete hashCache[hashCache.last.shift()];
-			}
-		};
-		return function sha1(data) {
-			var hash = hashCache[data];
-			if (hash) {
-				return hash;
-			}
-			hashCache[data] = hash = noCache(data);
-			hashCache.last.push(data);
-			hashCache.size = hashCache.size + 1;
-			shrinkToSize();
-			return hash;
-		};
-	};
-	return function sha1_init(cacheSize) {
-		switch(cacheSize) {
-			case 0:
-				return noCache;
-			case -1:
-				return allCache;
-			default:
-				return someCache(cacheSize);
-		}
+// Caches no results
+var noCache = function(data) {
+	return lib.crypto.createHash('sha1').update(data).digest('hex');
+};
+// Caches some results
+var allCache = (function() {
+	// Cache of hashed emails in a local scope.
+	var hashCache = {};
+	return function sha1(data) {
+		return hashCache[data] || hashCache[data] = noCache(data);
 	};
 }());
+// Caches a client-supplied number of results
+var someCache = function makeSomeHash(count) {
+	var hashCache = {
+		size: 0,
+		last: [],
+	};
+	var shrinkToSize = function() {
+		while (hashCache.size > count) {
+			delete hashCache[hashCache.last.shift()];
+		}
+	};
+	return function sha1(data) {
+		var hash = hashCache[data];
+		if (hash) {
+			return hash;
+		}
+		hashCache[data] = hash = noCache(data);
+		hashCache.last.push(data);
+		hashCache.size = hashCache.size + 1;
+		shrinkToSize();
+		return hash;
+	};
+};
+
+exports = function sha1_init(cacheSize) {
+	switch(cacheSize) {
+		case 0:
+			return noCache;
+		case -1:
+			return allCache;
+		default:
+			return someCache(cacheSize);
+	}
+};
